d3fb54b4079c861f7be3d5c2d2e5abae4e2bdc9c
Ivan Plantevin <ivan.p92@gmail.com>
2024-12-01 15:08:18 +0100

    solve day 1

--- src/main/kotlin/net/iplantevin/aoc/aoc2024/Day1.kt
+++ src/main/kotlin/net/iplantevin/aoc/aoc2024/Day1.kt
@@ -1,4 +1,35 @@
 package net.iplantevin.aoc.aoc2024
 
+import kotlin.math.abs
+
 object Day1 {
+
+    fun problem1a(input: String): Long {
+        val (leftList, rightList) = parseLists(input)
+        val leftSorted = leftList.sorted()
+        val rightSorted = rightList.sorted()
+        return leftSorted.foldIndexed(0L) { index, total, number ->
+            total + abs(number - rightSorted[index])
+        }
+    }
+
+    fun problem1b(input: String): Long {
+        val (leftList, rightList) = parseLists(input)
+        val counts = rightList.groupingBy { it }.eachCount()
+        return leftList.fold(0) { total: Long, number: Long ->
+            total + number * counts.getOrDefault(number, 0)
+        }
+    }
+
+    private fun parseLists(input: String): Pair<List<Long>, List<Long>> {
+        val leftList = mutableListOf<Long>()
+        val rightList = mutableListOf<Long>()
+        input.lineSequence().forEach { line ->
+            line.split("   ").let { (left, right) ->
+                leftList += left.toLong()
+                rightList += right.toLong()
+            }
+        }
+        return Pair(leftList, rightList)
+    }
 }
--- src/test/kotlin/net/iplantevin/aoc/aoc2024/AdventOfCode2024Test.kt
+++ src/test/kotlin/net/iplantevin/aoc/aoc2024/AdventOfCode2024Test.kt
@@ -1,5 +1,10 @@
 package net.iplantevin.aoc.aoc2024
 
+import io.kotest.matchers.shouldBe
+import net.iplantevin.aoc.aoc2024.Day1.problem1a
+import net.iplantevin.aoc.aoc2024.Day1.problem1b
+import net.iplantevin.aoc.util.input
+import net.iplantevin.aoc.util.timing
 import org.junit.jupiter.api.Test
 
 
@@ -7,6 +12,33 @@ class AdventOfCode2024Test {
 
     @Test
     fun part1a() {
+        problem1a(
+            """
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+        """.trimIndent()
+        ) shouldBe 11L
 
+        timing { problem1a(input(24, 1)) shouldBe 2086478L }
+    }
+
+    @Test
+    fun part1b() {
+        problem1b(
+            """
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+        """.trimIndent()
+        ) shouldBe 31L
+
+        timing { problem1b(input(24, 1)) shouldBe 24941624L }
     }
 }
