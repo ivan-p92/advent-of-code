8464242d23205dd90cd539f1b72846036ee3f2ed
Ivan Plantevin <ivan.p92@gmail.com>
2024-12-02 20:47:42 +0100

    solve day 2

---
+++ src/main/kotlin/net/iplantevin/aoc/aoc2024/Day2.kt
@@ -0,0 +1,40 @@
+package net.iplantevin.aoc.aoc2024
+
+import kotlin.math.abs
+
+object Day2 {
+
+    fun problem2a(input: String): Int = parse(input).count { reportIsSafe(it) }
+
+    fun problem2b(input: String): Int = parse(input).count { reportIsSafe(it, lenient = true) }
+
+    private fun parse(input: String) = input.lineSequence().map { it.split(" ").map(String::toInt) }
+
+    private fun reportIsSafe(report: List<Int>, lenient: Boolean = false): Boolean {
+        val isIncreasing = report[1] > report[0]
+
+        for (i in 0..<(report.size - 1)) {
+            val a = report[i]
+            val b = report[i + 1]
+            val isSafe = levelStepIsSafe(a, b, isIncreasing)
+            when {
+                !isSafe && lenient -> {
+                    val withoutPreviousIsSafe =
+                        if (i > 0) reportIsSafe(report.toMutableList().apply { removeAt(i - 1) }) else false
+                    return withoutPreviousIsSafe || reportIsSafe(report.toMutableList().apply { removeAt(i) }) ||
+                            reportIsSafe(report.toMutableList().apply { removeAt(i + 1) })
+                }
+
+                !isSafe -> return false
+            }
+        }
+        return true
+    }
+
+    private fun levelStepIsSafe(a: Int, b: Int, increasing: Boolean): Boolean = when {
+        a == b -> false
+        b > a && !increasing || b < a && increasing -> false
+        abs(a - b) > 3 -> false
+        else -> true
+    }
+}
--- src/test/kotlin/net/iplantevin/aoc/aoc2024/AdventOfCode2024Test.kt
+++ src/test/kotlin/net/iplantevin/aoc/aoc2024/AdventOfCode2024Test.kt
@@ -3,6 +3,8 @@ package net.iplantevin.aoc.aoc2024
 import io.kotest.matchers.shouldBe
 import net.iplantevin.aoc.aoc2024.Day1.problem1a
 import net.iplantevin.aoc.aoc2024.Day1.problem1b
+import net.iplantevin.aoc.aoc2024.Day2.problem2a
+import net.iplantevin.aoc.aoc2024.Day2.problem2b
 import net.iplantevin.aoc.util.input
 import net.iplantevin.aoc.util.timing
 import org.junit.jupiter.api.Test
@@ -40,5 +42,37 @@ class AdventOfCode2024Test {
         ) shouldBe 31L
 
         timing { problem1b(input(24, 1)) shouldBe 24941624L }
+    }
+
+    @Test
+    fun part2a() {
+        problem2a(
+            """
+            7 6 4 2 1
+            1 2 7 8 9
+            9 7 6 2 1
+            1 3 2 4 5
+            8 6 4 4 1
+            1 3 6 7 9
+        """.trimIndent()
+        ) shouldBe 2
+
+        timing { problem2a(input(24, 2)) shouldBe 663 }
+    }
+
+    @Test
+    fun part2b() {
+        problem2b(
+            """
+            7 6 4 2 1
+            1 2 7 8 9
+            9 7 6 2 1
+            1 3 2 4 5
+            8 6 4 4 1
+            1 3 6 7 9
+        """.trimIndent()
+        ) shouldBe 4
+
+        timing { problem2b(input(24, 2)) shouldBe 692 }
     }
 }
